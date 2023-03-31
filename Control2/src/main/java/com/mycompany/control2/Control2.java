d3a757565ba5dc15f6e241145800cdeb64eb96a1
VicenteHerreraM <119338193+VicenteHerreraM@users.noreply.github.com>
2023-03-30 22:33:26 -0300

    Update Control2.java

--- Control2/src/main/java/com/mycompany/control2/Control2.java
+++ Control2/src/main/java/com/mycompany/control2/Control2.java
@@ -9,8 +9,11 @@ package com.mycompany.control2;
  * @author vice1
  */
 public class Control2 {
-
+    static Colegio colegio = new Colegio;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cursos [] arregloCursos = colegio.cursosConAlumnosMenorPromedio();
+        for (int i = 0 ; i < arregloCursos.lenght ; i++){
+            System.out.println("Id curso: " +arreglo.Curso.getId() +" ");
+        }
     }
 }
